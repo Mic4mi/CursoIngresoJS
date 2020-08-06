525eafd1f9ab192033142cea7c4f82cb71815a16
Mic4mi <micahl1981@gmail.com>
2020-08-06 11:11:22 -0300

    Ejercicio de modelo de parcial

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
@@ -1,18 +1,4 @@
-
 function mostrar() {
-	// cargar 5 productos
-
-	// Cada producto tiene:
-	// tipo: Jabón, Barbijo, Alcohol. 
-	// precio: Entre 100 y 300
-	// cantidad: No puede ser cero o negativo ni superar las 1000 unidades
-	// Marca
-	// Fabricante
-
-	//Obtener
-	// A. Del alcohol más barato -> Cantidad de unidades y fabricante. 
-	// B. Del tipo (barbijo, jabón o alcohol) que tenga + unidades -> El promedio por compra
-	// C. Total de unidad de jabones
 	let cantidad;
 	let fabricante;
 	let marca;
