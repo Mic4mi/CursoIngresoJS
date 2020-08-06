185402a41e1d861c5b748e6d46c72b5d5046a06f
Mic4mi <micahl1981@gmail.com>
2020-08-06 19:42:58 -0300

    modelo de parcial

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
@@ -1,31 +1,111 @@
 function mostrar() {
-  //   Realizar el algoritmo que permita ingresar los datos de una compra productos de la construccion, hasta que el cliente quiera:
-  // Tipo validad("arena";"cal";"cemento")
-  // Cantidad de bolsas,
-  // Precio por bolsa (más de cero ),
-
-  // Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar.
-  // Si compro más de 30 bolsas en total tenes 25% de descuento sobre el total a pagar.
-  // a) El importe total a pagar , bruto sin descuento y...
-  // b) el importe total a pagar con descuento(solo si corresponde)
-  // d) Informar el tipo con mas cantidad de bolsas.
-  // f) El tipo mas caro
-
-  let tipo;
   let cantidad;
   let precioBolsa;
+  let tipo;
   let continuar = "si";
+  let cantidadTotalDeBolsas = 0;
+  let arenaCantidad = 0;
+  let calCantidad = 0;
+  let cementoCantidad = 0;
+  let totalBruto = 0;
+  let precioConQuinceDeDescuento = 0;
+  let precioConVeinticincoDeDescuento = 0;
+  let mensaje;
+  let tipoConMayorCantidad;
+  let arenaPrecio = 0;
+  let calPrecio = 0;
+  let cementoPrecio = 0;
+  let tipoConMayorPrecio;
 
 
   do {
     // Validar tipo
-    // tipo = prompt("Ingrese el tipo de producto: Arena, Cal o Cemento.").toLowerCase();
-    // while (tipo != "arena" && tipo != "cal" && tipo != "cemento") {
-    //   tipo = prompt("Incorrecto. Ingrese el tipo de producto: Arena, Cal o Cemento.").toLowerCase();
-    // }
-    // console.log("Tipo de producto: " + tipo);
+    tipo = prompt("Ingrese el tipo de producto: Arena, Cal o Cemento.").toLowerCase();
+    while (tipo != "arena" && tipo != "cal" && tipo != "cemento") {
+      tipo = prompt("Incorrecto. Ingrese el tipo de producto: Arena, Cal o Cemento.").toLowerCase();
+    }
 
-    // Validar cantidad
+    // Validar precio & cantidad
+    cantidad = parseInt(prompt("Ingrese la cantidad de bolsas."));
+    precioBolsa = parseFloat(prompt("Ingrese el precio por unidad."));
+    while (cantidad <= 0 || precioBolsa <= 0) {
+      if (cantidad <= 0) {
+        cantidad = parseInt(prompt("Incorrecto: La cantidad no puede ser cero o menor que cero. Ingrese la cantidad de bolsas."));
+      } else if (precioBolsa <= 0) {
+        precioBolsa = parseFloat(prompt("Incorrecto: El precio no puede ser menor o igual a cero. Ingrese el precio por unidad."));
+      }
+    }
+
+    switch (tipo) {
+      case "arena":
+        cantidadTotalDeBolsas += cantidad;
+        totalBruto += (precioBolsa * cantidad);
+        arenaCantidad += cantidad;
+        arenaPrecio += precioBolsa;
+        break;
+      case "cal":
+        cantidadTotalDeBolsas += cantidad;
+        totalBruto += (precioBolsa * cantidad);
+        calCantidad += cantidad;
+        calPrecio += precioBolsa;
+        break;
+      case "cemento":
+        cantidadTotalDeBolsas += cantidad;
+        totalBruto += (precioBolsa * cantidad);
+        cementoCantidad += cantidad;
+        cementoPrecio += precioBolsa;
+        break;
+    }
+
+    continuar = prompt("¿Desea ingresar otro producto?").toLowerCase();
+  } while (continuar == "si");
+
+  // DESCUENTO
+  console.log(cantidadTotalDeBolsas);
+  if (cantidadTotalDeBolsas >= 10 && cantidadTotalDeBolsas < 30) {
+    precioConQuinceDeDescuento = totalBruto - (totalBruto * 15) / 100;
+    mensaje = "Por tu compra mayor a 10 bolsas obtuviste un descuento del 15%. Precio a pagar: " + precioConQuinceDeDescuento;
+  } else if (cantidadTotalDeBolsas >= 30) {
+    precioConVeinticincoDeDescuento = totalBruto - (totalBruto * 25) / 100;
+    mensaje = "Por tu compra mayor a 30 bolsas obtuviste un descuento del 25%. Precio a pagar: " + precioConVeinticincoDeDescuento;
+  } else {
+    mensaje = "No hay descuento :("
+  }
+
+  // EL TIPO CON MÁS CANTIDAD
+  if (arenaCantidad > calCantidad && arenaCantidad > cementoCantidad) {
+    tipoConMayorCantidad = "El tipo con mayor cantidad es: Arena";
+  } else if (calCantidad >= arenaCantidad && calCantidad > cementoCantidad) {
+    tipoConMayorCantidad = "El tipo con mayor cantidad es: Cal";
+  } else {
+    tipoConMayorCantidad = "El tipo con mayor cantidad es: Cemento";
+  }
+
+  // EL TIPO CON MAYOR PRECIO
+  if (arenaPrecio > calPrecio && arenaPrecio > cementoPrecio) {
+    tipoConMayorPrecio = "El tipo con mayor precio es: Arena";
+  } else if (calPrecio >= arenaPrecio && calPrecio > cementoPrecio) {
+    tipoConMayorPrecio = "El tipo con mayor precio es: Cal";
+  } else {
+    tipoConMayorPrecio = "El tipo con mayor precio es: Cemento";
+  }
+
+  // Imprimir todo 
+  // TOTAL BRUTO
+  console.log("A. Total bruto: " + totalBruto);
+  // DESCUENTO
+  console.log("B. " + mensaje);
+  // TIPO CON MAYOR CANTIDAD
+  console.log("C. " + tipoConMayorCantidad);
+  // TIPO CON MAYOR PRECIO
+  console.log("D. " + tipoConMayorPrecio);
+
+}
+
+
+
+
+// Validar cantidad
     // cantidad = parseInt(prompt("Ingrese la cantidad de bolsas."));
     // while (cantidad <= 0) {
     //   cantidad = parseInt(prompt("Incorrecto: La cantidad no puede ser cero o menor que cero. Ingrese la cantidad de bolsas."));
@@ -38,23 +118,3 @@ function mostrar() {
     //   precioBolsa = parseFloat(prompt("Incorrecto: El precio no puede ser menor o igual a cero. Ingrese el precio por unidad."));
     // }
     // console.log("Precio por bolsa: " + precioBolsa);
-
-    cantidad = parseInt(prompt("Ingrese la cantidad de bolsas."));
-    precioBolsa = parseFloat(prompt("Ingrese el precio por unidad."));
-    while (cantidad <= 0 || precioBolsa <= 0) {
-      if (cantidad <= 0) {
-        cantidad = parseInt(prompt("Incorrecto: La cantidad no puede ser cero o menor que cero. Ingrese la cantidad de bolsas."));
-      } else if (precioBolsa <= 0) {
-        precioBolsa = parseFloat(prompt("Incorrecto: El precio no puede ser menor o igual a cero. Ingrese el precio por unidad."));
-      }
-    }
-    console.log("Cantidad de bolsas: " + cantidad);
-    console.log("Precio por bolsa: " + precioBolsa);
-
-    continuar = prompt("¿Desea ingresar otro proucto?").toLowerCase();
-  } while (continuar == "si");
-
-
-
-
-}
